743b5d8e79e725ecf7640683c516931adcffc14f
Paul Kathmann <pkathmann@palantir.com>
2026-09-19 17:27:01 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ impl ExecutableCommand {
 fn print_command_type(command_string: String) {
     let command = parse(&command_string);
     match command {
-        Command::Unknown { command: type_} => println!("{} not found", type_),
+        Command::Unknown { command: type_} => println!("{}: not found", type_),
         Command::Builtin(_)  => { println!("{} is a shell builtin",  command_string) },
         Command::Executable(_) => { println!("{} is a shell executable",  command_string) },
     }
