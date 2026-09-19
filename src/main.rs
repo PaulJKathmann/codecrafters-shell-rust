c7967bd83aafab58ae472cf84037868d7f0c942a
Paul Kathmann <pkathmann@palantir.com>
2026-09-18 20:53:45 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
-    let user_input = &mut String::new();
-    io::stdin().read_line(user_input).unwrap();
-    println!("{}: command not found", user_input.trim());
+    let mut user_input = String::new();
+    while let Ok(_) = io::stdin().read_line(&mut user_input) {
+        println!("{}: command not found", user_input.trim());
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        user_input.clear();
+    }
 }
